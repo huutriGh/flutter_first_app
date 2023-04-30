af3232903665891370053b6ec601fa5e3c822786
nguyen huu tri <huutriqt13@hotmail.com>
2023-04-30 19:08:59 +0700

    add display image, multiple contrucstor, button, styling button

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,13 @@
+import 'package:first_app/gradient_container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     home: Scaffold(
-      body: GradientContainer(),
+      body: GradientContainer(
+        const Color.fromARGB(255, 33, 5, 109),
+        const Color.fromARGB(255, 33, 5, 109),
+      ),
     ),
   ));
-}
-
-class GradientContainer extends StatelessWidget {
-  // GradientContainer({key}) : super(key: key);
-  const GradientContainer({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            Color.fromARGB(255, 26, 2, 80),
-            Color.fromARGB(255, 45, 7, 98),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-        ),
-      ),
-      child: const Center(
-        child: Text(
-          "Hello World!",
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 28,
-          ),
-        ),
-      ),
-    );
-  }
 }
