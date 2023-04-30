2a3635a66155ba5dd0ca01f23097af2d899d6a6c
nguyen huu tri <huutriqt13@hotmail.com>
2023-04-30 19:50:06 +0700

    add statefull widget

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,3 +1,4 @@
+import 'package:first_app/dice_roller.dart';
 import 'package:flutter/material.dart';
 
 const startAlignment = Alignment.topLeft;
@@ -25,16 +26,12 @@ const endAlignment = Alignment.bottomRight;
 // }
 class GradientContainer extends StatelessWidget {
   // GradientContainer({key}) : super(key: key);
-  GradientContainer(this.color1, this.color2, {super.key});
-  GradientContainer.purple({super.key})
+  const GradientContainer(this.color1, this.color2, {super.key});
+  const GradientContainer.purple({super.key})
       : color1 = Colors.deepPurple,
         color2 = Colors.indigo;
   final Color color1;
   final Color color2;
-  var activeDiceImage = 'assets/images/dice-2.png';
-  void rollDice() {
-    activeDiceImage = 'assets/images/dice-4.png';
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -46,26 +43,8 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              'assets/images/dice-2.png',
-              width: 200,
-            ),
-            const SizedBox(height: 20),
-            TextButton(
-              onPressed: rollDice,
-              style: TextButton.styleFrom(
-                // padding: const EdgeInsets.only(top: 20),
-                foregroundColor: Colors.white,
-                textStyle: const TextStyle(fontSize: 28),
-              ),
-              child: const Text('Role Dice'),
-            ),
-          ],
-        ),
+      child: const Center(
+        child: DiceRoller(),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,11 @@ import 'package:first_app/gradient_container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(const MaterialApp(
     home: Scaffold(
       body: GradientContainer(
-        const Color.fromARGB(255, 33, 5, 109),
-        const Color.fromARGB(255, 33, 5, 109),
+        Color.fromARGB(255, 33, 5, 109),
+        Color.fromARGB(255, 33, 5, 109),
       ),
     ),
   ));
